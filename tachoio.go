1bae8f0c7b8f3b9ea174e14f3a4c5d6364e3124c
Homin Lee <homin.lee@suapapa.net>
2016-11-21 22:50:29 +0900

    Initial commit

---
+++ tachoio.go
@@ -0,0 +1,76 @@
+package tachoio
+
+import (
+	"io"
+	"sync"
+	"time"
+)
+
+type Reader struct {
+	ts  time.Time
+	cnt int
+	rd  io.Reader
+
+	sync.Mutex
+}
+
+// NewReader retrurs a new Reader
+func NewReader(rd io.Reader) *Reader {
+	r := new(Reader)
+	r.ts = time.Now()
+	return r
+}
+
+func (r *Reader) Read(p []byte) (n int, err error) {
+	r.Lock()
+	defer r.Unlock()
+
+	n, err = r.rd.Read(p)
+	r.cnt += n
+	return
+}
+
+func (r *Reader) ReadMeter() (n int, d time.Duration) {
+	r.RLock()
+	defer r.Unlock()
+
+	d = time.Since(r.ts)
+	r.ts = time.Now()
+	n = r.cnt
+	r.cnt = 0
+	return
+}
+
+type Writer struct {
+	ts  time.Time
+	cnt int
+	wr  io.Writer
+
+	sync.Mutex
+}
+
+// NewWriter returns a new Writer
+func NewWriter(wr io.Writer) *Writer {
+	w := new(Reader)
+	return w
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+	w.Lock()
+	defer w.Unlock()
+
+	n, err = w.wr.Write(p)
+	w.cnt += n
+	return
+}
+
+func (w *Write) WriteMeter() (n int, d time.Duration) {
+	w.Lock()
+	defer w.Unlock()
+
+	d = time.Since(w.ts)
+	w.ts = time.Now()
+	n = w.cnt
+	w.cnt = 0
+	return
+}
