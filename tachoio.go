c7592550504e82e31df5bd4246d6660591c30632
Homin Lee <homin.lee@suapapa.net>
2016-11-24 19:56:44 +0900

    add LICENSE

--- tachoio.go
+++ tachoio.go
@@ -1,4 +1,8 @@
-package tachoio
+// Copyright 2016, Homin Lee <homin.lee@suapapa.net>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package tachoio // import "github.com/suapapa/go_tachoio"
 
 import (
 	"fmt"
